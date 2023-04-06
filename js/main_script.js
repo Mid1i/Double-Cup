e45f9b5808bdd024c7906c488371a9c1ad1e14ca
Mid1i <michac.tchernov@yandex.ru>
2023-04-07 01:01:01 +0300

    added fruits section

--- js/main_script.js
+++ js/main_script.js
@@ -1,6 +1,9 @@
+const header_list = document.getElementsByClassName("header-list")[0];
+const body = document.querySelector("body");
+
 document.addEventListener('click', (event) => {
     if (event.target.classList.contains('header-menu-burger')) {
-        $(".header-list").toggleClass("active"); 
-        $("body").toggleClass("lock");   
+        header_list.classList.toggle("active"); 
+        body.classList.toggle("lock");   
     }
 });
--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(() => {
     $('.slider').slick({
         arrows: false,
         autoplay: true,
@@ -11,4 +11,32 @@ $(document).ready(function() {
         swipe: false,
         waitForAnimation: true
     });
+    ProductSlider();
 });
+
+function ProductSlider() {
+    if (window.matchMedia("(max-width: 520px)").matches) {
+        $(".products").not('.slick-initialized').slick({
+            responsive: [
+                {
+                    breakpoint: 9999,
+                    settings: "unslick"
+                },
+                {
+                    breakpoint: 520,
+                    settings: {
+                        arrows: true,
+                        dots: false,
+                        easing: 'linear',
+                        slidesToShow: 1,
+                        speed: 800,
+                        swipe: true,
+                        waitForAnimation: true
+                    }
+                }
+            ]
+        });
+    };  
+};
+
+window.onresize = () => ProductSlider();
