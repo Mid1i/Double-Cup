33739842a5d2f34ad41dc691767ec55ce3cf59f2
Mid1i <michac.tchernov@yandex.ru>
2023-04-22 13:11:02 +0300

    fixed scale

--- main_page/js/main_script.js
+++ main_page/js/main_script.js
@@ -36,23 +36,6 @@ window.addEventListener("resize", () => {
     productSlider();
 });
 
-(function($) {
-    $.fn.nodoubletapzoom = function() {
-        $(this).bind('touchstart', function preventZoom(e) {
-          var t2 = e.timeStamp
-            , t1 = $(this).data('lastTouch') || t2
-            , dt = t2 - t1
-            , fingers = e.originalEvent.touches.length;
-          $(this).data('lastTouch', t2);
-          if (!dt || dt > 500 || fingers > 1) return; // not double-tap
-  
-          e.preventDefault(); // double tap - prevent the zoom
-          // also synthesize click events we just swallowed up
-          $(this).trigger('click').trigger('click');
-        });
-    };
-  })
-
 document.addEventListener("click", (event) => {
     let arg = event.target;
     let id = arg.id;
