3facf93f10dee1a78d61edef3fdcc5ae8269757c
Mid1i <michac.tchernov@yandex.ru>
2023-04-29 19:10:26 +0300

    added new files and fix bugs

---
+++ error_page/js/createCart.js
@@ -0,0 +1,120 @@
+export const cart_block_items = document.querySelector(".cart-block__items");
+
+export let cart = {
+    "apples": 0,
+    "bananas": 0,
+    "cucumbers": 0,
+    "tomatoes": 0,
+    "white-bread": 0,
+    "croissant": 0,
+    "chiabatta": 0,
+    "black-bread": 0,
+    "donuts": 0,
+    "macarons": 0,
+    "marmalade": 0,
+    "cookies": 0,
+    "pasta-spirals": 0,
+    "pasta-feather": 0,
+    "buckwheat": 0,
+    "rice": 0,
+    "rus-cheese": 0,
+    "hol-cheese": 0,
+    "boil-sausage": 0,
+    "rs-sausage": 0,
+    "milk": 0,
+    "curd": 0,
+    "sour-cream": 0,
+    "eggs": 0
+};
+
+let data;
+
+// Getting the JSON file of goods
+$.getJSON("https://mid1i.github.io/Double-Cup/resourses/data/data.json", function(json) {
+    data = json;
+});
+
+// Getting the product info from JSON
+export function getProduct(id) {
+    let product;
+
+    data.forEach((item) => {
+        if (item.id == id) {
+            product = item;
+        }
+    });
+
+    return product;
+}
+
+// Adding the product section to the DOM
+export function createCartItem(id) {
+    let product = getProduct(id);
+
+    if (product) {
+        let tree = createStructure(product);
+
+        let cart_item = document.createElement("section");
+        cart_item.classList.add("cart-block__item", `${id}-cart-item`);
+        cart_item.appendChild(tree);
+    
+        cart_block_items.appendChild(cart_item);
+    }
+}
+
+// Creating a product section in the Cart
+export function createStructure(product) {
+    let image = document.createElement("img");
+    image.src = product.image;
+
+    let image_wrapper = document.createElement("div");
+    image_wrapper.classList.add("item__image");
+    image_wrapper.appendChild(image);
+
+    let title = document.createElement("h4");
+    title.classList.add("item__title");
+    title.innerHTML = product.name;
+
+    let counter = document.createElement("div");
+    counter.classList.add("item__counter", `js-${product.id}-counter`);
+    counter.id = product.id;
+    counter.innerHTML = cart[product.id];
+
+    let minus_button = document.createElement("button");
+    minus_button.classList.add("item__btn", "minus-btn", `js-${product.id}-minus`);
+    minus_button.id = product.id;
+    minus_button.innerHTML = "—";
+
+    let plus_button = document.createElement("button");
+    plus_button.classList.add("item__btn", "plus-btn", `js-${product.id}-plus`);
+    plus_button.id = product.id;
+    plus_button.innerHTML = "+";
+
+    let buttons = document.createElement("div");
+    buttons.classList.add("item__btns-block");
+    buttons.append(counter, minus_button, plus_button);
+
+    let price = document.createElement("div");
+    price.classList.add("item__price", `js-${product.id}-price`);
+    price.innerHTML = `${cart[product.id] * product.price}.00 ₽`;
+
+    let delete_button = document.createElement("div");
+    delete_button.classList.add("item__delete", `js-${product.id}-delete`);
+    delete_button.id = product.id;
+    delete_button.innerHTML = "Удалить";
+
+    let tree = document.createDocumentFragment();
+    tree.append(image_wrapper, title, buttons, price, delete_button);
+
+    return tree;
+}
+
+export function startSettings(str) {
+    cart = JSON.parse(localStorage.getItem(str));
+
+    for (let item in cart) {
+        if (cart[item] != 0) {
+            createCartItem(item);
+        }
+    }
+}
--- error_page/js/main_script.js
+++ error_page/js/main_script.js
@@ -1,5 +1,7 @@
+import { createLoginButton, createProfileButton } from "./createProfile.js";
+
 const header_list = document.querySelector(".main-header__list"); 
-const body = document.querySelector("body"); 
+const body = document.querySelector("body");
 
 const login = document.querySelector(".js-login__block");
 const register = document.querySelector(".js-register__block");
@@ -12,13 +14,19 @@ const email = document.querySelector(".form__input_email");
 const password = document.querySelector(".form__input_pass");
 const checkbox = document.querySelector(".form__checkbox_input");
 
-const username_login = document.querySelector(".js-name-login");
-const password_login = document.querySelector(".js-password-login");
+let userNameItem = localStorage.getItem("username");
+
+// Creating the "Login" or "Profile" button
+if (userNameItem) {
+    createProfileButton(userNameItem);
+} else {
+    createLoginButton();
+}
 
 document.addEventListener("click", (event) => {
     let arg = event.target;
     let id = arg.id;
-    
+
     // Tracks clicking on the "Menu" button
     if (arg.classList.contains("main-header__burger")) {
         document.querySelector(".main-header__burger").classList.toggle("active"); 
@@ -29,6 +37,12 @@ document.addEventListener("click", (event) => {
     // Tracks clicking on the "Login" button
     if (arg.classList.contains("js-login__btn")) {
         login.classList.add("active_profile");
+    }
+
+    // Tracks clicking on the "Cross" button when closing the login window
+    if (arg.classList.contains("js-quit")) {
+        login.classList.remove("active_profile"); 
+        register.classList.remove("active_profile");
     }
 
     // Tracks clicking on the "Dont have an account" button
@@ -46,11 +60,6 @@ document.addEventListener("click", (event) => {
     // Tracks clicking on the "Register" button
     if ((id == "register-btn") && (![username.value.trim(), email.value.trim(), password.value.trim()].includes("")) && checkbox.checked) {
         localStorage.setItem("username", username.value.trim());
-    }
-
-    // Tracks clicking on the "Login" button
-    if ((arg.classList.contains("form__btn_login")) && (![username_login.value.trim(), password_login.value.trim()].includes(""))) {
-        localStorage.setItem("username", username_login.value.trim());
     }
 });
 
